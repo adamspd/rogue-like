9a71e42d156db75b0f1657182caa1a31cf81b3b2
Adams Pierre David <adamspierredavid@gmail.com>
2021-05-03 21:17:02 +0200

    Optimisation: rajout de getter et de setter

--- src/main/java/rogue2/entite/AbstractEntity.java
+++ src/main/java/rogue2/entite/AbstractEntity.java
@@ -3,8 +3,39 @@ package rogue2.entite;
 import rogue0.utils.Pos;
 
 public abstract class AbstractEntity {
-    private Pos potion;
-    private int PV;
+    private Pos position;
+    private int LifePoints;
     private final double movement;
-    private final int damage;
+    private final int damages;
+
+    public AbstractEntity(Pos position, int lifePoints, double movement, int damages) {
+        this.position = position;
+        LifePoints = lifePoints;
+        this.movement = movement;
+        this.damages = damages;
+    }
+
+    public Pos getPosition() {
+        return position;
+    }
+
+    public void setPosition(Pos position) {
+        this.position = position;
+    }
+
+    public int getLifePoints() {
+        return LifePoints;
+    }
+
+    public void setLifePoints(int lifePoints) {
+        LifePoints = lifePoints;
+    }
+
+    public double getMovement() {
+        return movement;
+    }
+
+    public int getDamages() {
+        return damages;
+    }
 }
