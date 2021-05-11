06eca8c77d111ef0feee8810ddad9c1d58259c45
davidPRIGODIN <79266421+davidPRIGODIN@users.noreply.github.com>
2021-05-11 16:34:15 +0200

    genererateStairs() Gestion cas: Pas d'espace

--- src/main/java/rogue3/artefact/Event.java
+++ src/main/java/rogue3/artefact/Event.java
@@ -16,14 +16,17 @@ public class Event  {
     public static int posX_stairs, posY_stairs; // Haut de l'escalier.
     public static int stairs_length = 3;
     public static boolean isCalled_ifMonstersAreAllDead_ThenUpperLevelEntryOpen;
+    private static int entier_si_pas_espace = 0;
 
+
+    //Quand on a l'entier_si_pas_espace, on ne regarde que la 1ère case au dessus de la salle
     private static boolean freeSpace(Grille grille, Salle salle_aleatoire, int posX) {
         if(grille.getSymbolAtCoord(new Position(posX, salle_aleatoire.getPos().getY()))==Coffre.getSymbole() ||
                 grille.getSymbolAtCoord(new Position(posX, salle_aleatoire.getPos().getY()))==Portal.getSymbole() ||
                 grille.getSymbolAtCoord(new Position(posX, salle_aleatoire.getPos().getY()))=="K ")
         {return false;}
-        for (int j = -4; j < 0; j++) {
-            for (int i = -1; i < 2; i++) { //Un espace à gauche et à droite
+        for (int j = -1-stairs_length + entier_si_pas_espace; j < 0; j++) {
+            for (int i = -1 + entier_si_pas_espace; i < 2 - entier_si_pas_espace; i++) { //Un espace à gauche et à droite
                 try {
                     if (grille.getSymbolAtCoord(new Position(posX + i, salle_aleatoire.getPos().getY() + j)) != grille.getSymbolGrille()) { return false; }
                 }
@@ -33,7 +36,6 @@ public class Event  {
         return true;
     }
 
-
     public static void genererateStairs(Grille grille) {    //Escalier présent, mais caché et inaccessible
         /*On choisit une salle.
         On choisit une position, en bordure de salle.
@@ -42,36 +44,47 @@ public class Event  {
         ArrayList<Salle> listeSallesDisponibles = grille.getListOfSalle();
         Salle salle_aleatoire;
         int posX;
+
+
         //Recherche d' un espace libre
-        do {
-            //On prend une salle aléatoire
-            salle_aleatoire = listeSallesDisponibles.get((int) (Math.random() * listeSallesDisponibles.size()));
-            //Enregistrement des positions de la salle (posX_Salle)
-            ArrayList<Integer> posX_Salle = new ArrayList<Integer>();
-            for (int i = (int)salle_aleatoire.getPos().getX() + 1; i < salle_aleatoire.getPos().getX() + salle_aleatoire.getSalleWidth() - 2; i++) {
-                posX_Salle.add(i);
-            }
-
+        try {
             do {
-                //On prend une position aléatoire
-                posX = posX_Salle.get((int) (Math.random() * posX_Salle.size()));
-                freeSpace = freeSpace(grille, salle_aleatoire, posX);
-                if (freeSpace) { break; }
-                posX_Salle.remove(Integer.valueOf(posX));
-            }
-            while (!posX_Salle.isEmpty());
-            listeSallesDisponibles.remove(salle_aleatoire);
-        }
-        while (!freeSpace);
-        posX_stairs= posX;
-        posY_stairs= salle_aleatoire.getPos().getY()-stairs_length;
-    }
+                //On prend une salle aléatoire
+                salle_aleatoire = listeSallesDisponibles.get((int) (Math.random() * listeSallesDisponibles.size()));
+                //Enregistrement des positions de la salle (posX_Salle)
+                ArrayList<Integer> posX_Salle = new ArrayList<Integer>();
+                for (int i = (int) salle_aleatoire.getPos().getX() + 1; i < salle_aleatoire.getPos().getX() + salle_aleatoire.getSalleWidth() - 2; i++) {
+                    posX_Salle.add(i);
+                }
 
+                do {
+                    //On prend une position aléatoire
+                    posX = posX_Salle.get((int) (Math.random() * posX_Salle.size()));
+                    freeSpace = freeSpace(grille, salle_aleatoire, posX);
+                    if (freeSpace) {
+                        break;
+                    }
+                    posX_Salle.remove(Integer.valueOf(posX));
+                }
+                while (!posX_Salle.isEmpty());
+                listeSallesDisponibles.remove(salle_aleatoire);
+            }
+            while (!freeSpace);
+            posX_stairs= posX;
+            posY_stairs= salle_aleatoire.getPos().getY()-stairs_length;
+        }
+        //Si on n'en trouve pas, la longueur de l'escalier est réduite au minimum.
+        catch (Exception e){
+            entier_si_pas_espace = 1;
+            stairs_length = 1;
+            genererateStairs(grille);
+        }
+    }
 
 
     public static void ifMonstersAreAllDead_ThenUpperLevelEntryOpen(Grille grille, Draw draw){     //Rend l'escalier visible
         if(grille.getListMonster().isEmpty() && !isCalled_ifMonstersAreAllDead_ThenUpperLevelEntryOpen){
-            for (int posY = posY_stairs; posY < posY_stairs + 3; posY++) {
+            for (int posY = posY_stairs; posY < posY_stairs + stairs_length; posY++) {
                 grille.addElement(new Position(posX_stairs, posY) , stairs_symbol);
             }
             isCalled_ifMonstersAreAllDead_ThenUpperLevelEntryOpen = true;
@@ -81,11 +94,13 @@ public class Event  {
 
     public static void ifPlayerHasGoneThroughTheUpperLevelEntry_ThenGenerateNewMap(Grille grille, Player player, Map map, Draw draw){
         if(grille.getSymbolAtCoord(new Position(posX_stairs,posY_stairs))==player.getSymbol()){
-            grille.reset(map, player);
+            entier_si_pas_espace = 0;
+            stairs_length = 3;
             isCalled_ifMonstersAreAllDead_ThenUpperLevelEntryOpen= false;
-            Information.NOMBRE_MONSTRES_CONNU= grille.getListMonster().size();
+            grille.reset(map,player);
             map.NIVEAU++;
             Information.liste_infos.add("NIVEAU " + map.NIVEAU);
+            Information.NOMBRE_MONSTRES_CONNU= grille.getListMonster().size();
             draw.draw(grille);
         }
     }
